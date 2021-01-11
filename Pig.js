0c0641d5575466656ba5efda5a919130a1007252
divyavala <divyavala@users.noreply.github.com>
2021-01-11 21:03:13 +0530

    Add files via upload

---
+++ Pig.js
@@ -0,0 +1,43 @@
+class Pig extends BaseClass {
+  constructor(x, y){
+    super(x,y,50,50);
+    this.image = loadImage("sprites/enemy.png");
+//this.remove=false
+
+ this.Visiblity=255;
+  }
+display()
+{
+  //console.log(this.body.)
+if((this.body.speed)<2.5)
+{super.display();
+}else
+{
+World.remove(world,this.body)  
+
+//if(this.remove===false){
+  //score=score+50
+  //this.remove=true
+//}
+push();
+this.Visiblity=this.Visiblity-5;
+tint(255,this.Visiblity)
+
+//do nothing
+image(this.image,this.body.position.x,this.body.position.y,50,50)
+pop();
+}}
+
+
+score(){
+
+
+
+  if(this.Visiblity<0&& this.Visiblity>-1005){
+
+    score=score+1
+  }
+}
+};
+
+
